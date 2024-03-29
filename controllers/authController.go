48b17096721d8a91cae5eff24f7f00e5d102dc7d
web3santa <usefulstory35@gmail.com>
2024-03-29 21:43:35 +0900

    finished golang jwt backend

--- controllers/authController.go
+++ controllers/authController.go
@@ -73,7 +73,7 @@ func Login(c *fiber.Ctx) error {
 	godotenv.Load()
 	SECRET_KEY := os.Getenv("SECRET_KEY")
 
-	tokenString, err := claims.SignedString([]byte(SECRET_KEY))
+	token, err := claims.SignedString([]byte(SECRET_KEY))
 
 	if err != nil {
 		// JWT 토큰 생성에 실패한 경우
@@ -83,6 +83,63 @@ func Login(c *fiber.Ctx) error {
 		})
 	}
 
+	cookie := fiber.Cookie{
+		Name:     "jwt",
+		Value:    token,
+		Expires:  time.Now().Add(24 * time.Hour),
+		HTTPOnly: true,
+	}
+
+	c.Cookie(&cookie)
+
 	// JWT 토큰 생성에 성공한 경우
-	return c.JSON(tokenString)
+	return c.JSON(fiber.Map{
+		"message": "success",
+	})
+}
+
+func User(c *fiber.Ctx) error {
+	cookie := c.Cookies("jwt")
+	godotenv.Load()
+	SECRET_KEY := os.Getenv("SECRET_KEY")
+
+	token, err := jwt.ParseWithClaims(cookie, &jwt.RegisteredClaims{}, func(t *jwt.Token) (interface{}, error) {
+		return []byte(SECRET_KEY), nil
+	})
+	if err != nil {
+		c.Status(fiber.StatusUnauthorized) // 500 상태 코드 반환
+		return c.JSON(fiber.Map{
+			"message": "unauthenticated", // 오류 메시지를 JSON 응답에 포함
+		})
+	}
+
+	claims := token.Claims
+
+	str, err := claims.GetIssuer()
+	if err != nil {
+		c.Status(fiber.StatusBadRequest) // 500 상태 코드 반환
+		return c.JSON(fiber.Map{
+			"message": "GetIssuer error", // 오류 메시지를 JSON 응답에 포함
+		})
+	}
+
+	var user model.User
+	database.DB.Where("id = ?", str).First(&user)
+
+	return c.JSON(user)
+}
+
+func Logout(c *fiber.Ctx) error {
+	cookie := fiber.Cookie{
+		Name:     "jwt",
+		Value:    "",
+		Expires:  time.Now().Add(-time.Hour),
+		HTTPOnly: true,
+	}
+
+	c.Cookie(&cookie)
+
+	return c.JSON(fiber.Map{
+		"message": "success",
+	})
 }
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/web3santa/JWT-Authentication-Golang/database"
 	"github.com/web3santa/JWT-Authentication-Golang/routes"
 )
@@ -9,6 +10,12 @@ import (
 func main() {
 	database.Connect()
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowCredentials: true,
+		AllowOrigins:     "http://127.0.0.1:3000",
+	}))
+
 	routes.Setup(app)
 
 	app.Listen(":3000")
